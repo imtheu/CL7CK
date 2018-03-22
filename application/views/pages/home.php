ac46ae18f360c10e0a470c35a3e298576b6b7213
Matheus <matheusfrmorais@hotmail.com>
2018-03-22 19:51:08 -0300

    First commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,37 @@
+
+<div class="jumbotron with-bg">
+	<div class="container">
+		<h1 class="display-4">Reach your goals. Now.</h1>
+		<p class="lead">No more procrastination. With Cl7ck you can finally organize your time.</p>
+		<p>It's awesome, it's open source, it's free!</p>
+		<p class="lead">
+			<a class="btn btn-primary btn-lg" href="#" role="button">Create an account</a>
+		</p>
+	</div>
+</div>
+
+<main>
+	<div class="container text-center">
+		<h1 class="h2 text-center text-uppercase">You can do amazing things if you dedicate your time to it.</h1>
+		<span class="lead"><em>Simple as that.</em></span>
+		<p>Procrastination is pointed out by several studies as the primary evil among students and workers. <br/>To avoid this problem CL7CK helps you to</p>
+		<div style="display: inline-block; vertical-align: text-top; padding: 50px 0; color: #2196f3" class="text-center">
+			<i class="fas fa-check fa-5x"></i>
+			<br><br>
+			<span>Set clear goals</span>
+		</div>
+		<div style="display: inline-block; vertical-align: text-top; padding: 50px 50px; color: #2196f3" class="text-center">
+			<i class="fas fa-list-ol fa-5x"></i>
+			<br><br>
+			<span>List the tasks to be done</span>
+		</div>
+		<div style="display: inline-block; vertical-align: text-top; padding: 50px 0; color: #2196f3" class="text-center">
+			<i class="far fa-calendar fa-5x"></i>
+			<br><br>
+			<span>Determine schedules</span>
+		</div>
+	</div>
+	<div class="jumbotron bg-primary text-white text-center mb-0">
+			<span class="display-3">It's free! :P</span>
+	</div>
+</main>
---
+++ application/views/template/footer.php
@@ -0,0 +1,5 @@
+<div class="bg-dark text-white text-center">
+	<a class="navbar-brand" href="<?php echo base_url() ?>"><img src="<?php echo base_url() ?>assets/img/logo.png" alt="Cl7ck" width="150"></a>
+
+	<span>Made with &hearts; in Minas Gerais.</span>
+</div>
